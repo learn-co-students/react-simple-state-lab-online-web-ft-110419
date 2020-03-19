16887c0e3f5aa0cb25226b0e506a7ca0bb21f7ec
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-19 01:12:37 -0400

    Done.

---
+++ src/Cell.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class Cell extends Component {
+    constructor(props) {
+        super()
+        this.state = { color: props }
+    }
+
+    updateColor() {
+        this.setState(
+            color = '#333'
+        )
+    }
+    render() {
+        return <div className='cell' style={{backgroundColor: 'this.state.color'}}></div>
+    }
+}
