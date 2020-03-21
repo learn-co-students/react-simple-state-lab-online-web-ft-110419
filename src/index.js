4bc57a4f28b37cd68c4416eb764a43a34e7981d0
AnnaWijetunga <annawijetunga@gmail.com>
2020-03-21 07:35:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import './index.css';
 import { pattern1 } from './data.js'
 import Matrix from './Matrix.js'
 
+// Matrix gets a values props of pattern1
+// which is imported from data.js
 ReactDOM.render(<Matrix values={pattern1} />, document.getElementById('root'));
